a58d7e4f2d049f855c4391cb4cf2f954646fabc7
msk200224 <kotoura@liquitous.com>
2022-05-23 15:18:46 +0900

    Update input.py

--- input.py
+++ input.py
@@ -2,12 +2,12 @@ import sympy
  
 # Input the graph
 # リンクとノードの数を入力
-link_length = input("リンクの数を入力:")
-node_length = input("ノードの数を入力:")
+link_length = int(input("リンクの数を入力:"))
+node_length = int(input("ノードの数を入力:"))
 
 # 各リンクごとの接続ノードを入力
 link = {}
-for i in range(int(link_length)):
+for i in range(link_length):
     link[int(i)] = [0,0]
     link[int(i)][0] = int(input("リンク"+str(i)+"番の接続ノードを入力:"))
     link[int(i)][1] = int(input("リンク"+str(i)+"番の接続ノードを入力:"))
@@ -17,11 +17,15 @@ print(link)
 # xを算出
 x, y, a, b, s = sympy.symbols('x,y,a,b,s')
 a = 1.7
-b = int(link_length)
+b = link_length
 s = 30
-f = a^(int(b)-1)*x - s
-ans = sympy.solve(f)[0]
-print("信頼性は"+ans)
+f = (a**(b-1))*x - s
+x = sympy.solve(f)
+print("x="+str(x[0]))
+
+# sが正しく復号できるか検算
+g = float((a**(b-1))*x[0])
+print("s="+str(g))
 
 """
 # xを算出(ans)
